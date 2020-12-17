46bb6180b682e202c2f955b904e3ac27c177cdbc
liucehngyiu <wanchengquan@hzsaiyue.com>
2020-12-17 14:59:43 +0800

    change to arm-gcc

--- ec_xdr_interface.h
+++ ec_xdr_interface.h
@@ -1,4 +1,13 @@
+#ifndef EC_XDR_INTERFACE_H
+#define EC_XDR_INTERFACE_H
 #include "stdio.h"
 #include "stdlib.h"
-char* ec_xdr_decode(unsigned char* payload, int length, int prm);
-unsigned char * ec_xdr_encode(char* payload, int* len, char* tag);
+#ifdef __cplusplus
+extern "C" { 
+#endif
+    char* ec_xdr_decode(unsigned char* payload, int length, int prm);
+    unsigned char * ec_xdr_encode(char* payload, int* len, char* tag);
+#ifdef __cplusplus
+};
+#endif
+#endif
